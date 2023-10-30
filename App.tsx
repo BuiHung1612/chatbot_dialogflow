a394a0ceb8b763db81e61fce8f51634c18e24c03
HungBui1612 <hungbv@osp.com>
2023-10-30 17:32:09 +0700

    chore: update UI

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
-import React, {useState, useCallback, useEffect, useRef} from 'react';
+import React, {useState, useCallback, useEffect} from 'react';
 import {
-  SafeAreaView,
   PermissionsAndroid,
+  SafeAreaView,
   Platform,
   Button,
-  EmitterSubscription,
+  Text,
+  View,
+  StyleSheet,
 } from 'react-native';
 import {GiftedChat, IMessage} from 'react-native-gifted-chat';
 import Voice, {
@@ -17,33 +19,36 @@ import Voice, {
 
 const App = () => {
   const [messages, setMessages] = useState<IMessage[]>([]);
-  const [showRecordButton, setShowRecordButton] = useState(false);
   const [isStartedMicro, setIsStartedMicro] = useState(false);
-  const recordEventListenner = useRef<EmitterSubscription>();
-  const requestMicro = async () => {
-    try {
-      const granted = await PermissionsAndroid.request(
-        PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
-        {
-          title: 'Cool Photo App Camera Permission',
-          message:
-            'Cool Photo App needs access to your camera ' +
-            'so you can take awesome pictures.',
-          buttonNeutral: 'Ask Me Later',
-          buttonNegative: 'Cancel',
-          buttonPositive: 'OK',
-        },
-      );
 
-      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-        setShowRecordButton(true);
-        console.log('You can use the micro');
-      } else {
-        setShowRecordButton(false);
-        console.log('Micro permission denied');
+  const requestMicro = async () => {
+    if (Platform.OS === 'android') {
+      try {
+        const grants = await PermissionsAndroid.requestMultiple([
+          PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+          PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+          PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
+        ]);
+
+        console.log('write external stroage', grants);
+
+        if (
+          grants['android.permission.WRITE_EXTERNAL_STORAGE'] ===
+            PermissionsAndroid.RESULTS.GRANTED &&
+          grants['android.permission.READ_EXTERNAL_STORAGE'] ===
+            PermissionsAndroid.RESULTS.GRANTED &&
+          grants['android.permission.RECORD_AUDIO'] ===
+            PermissionsAndroid.RESULTS.GRANTED
+        ) {
+          console.log('Permissions granted');
+        } else {
+          console.log('All required permissions not granted');
+          return;
+        }
+      } catch (err) {
+        console.warn(err);
+        return;
       }
-    } catch (err) {
-      console.warn(err);
     }
   };
 
@@ -53,10 +58,6 @@ const App = () => {
     Voice.onSpeechEnd = onSpeechEnd;
     Voice.onSpeechError = onSpeechError;
     Voice.onSpeechResults = onSpeechResults;
-
-    // Voice.onSpeechPartialResults = onSpeechPartialResults;
-    // Voice.onSpeechVolumeChanged = onSpeechVolumeChanged;
-
     return () => {
       Voice.destroy().then(Voice.removeAllListeners);
     };
@@ -77,6 +78,22 @@ const App = () => {
   };
   const onSpeechResults = (e: SpeechResultsEvent) => {
     console.log('onSpeechResults', e);
+    setIsStartedMicro(false);
+    if (e != null && e.value != null) {
+      const messages = [
+        {
+          _id: Date.now(),
+          text: e.value[0],
+          createdAt: Date.now(),
+          user: {
+            _id: 1,
+            name: 'Me',
+          },
+        },
+      ];
+      onSend(messages);
+      sendRequest(messages[0]);
+    }
   };
   useEffect(() => {
     if (Platform.OS === 'android') {
@@ -84,21 +101,7 @@ const App = () => {
     }
   }, []);
 
-  useEffect(() => {
-    setMessages([
-      {
-        _id: 1,
-        text: 'Hello developer',
-        createdAt: new Date(),
-        user: {
-          _id: 2,
-          name: 'React Native',
-          avatar: 'https://placeimg.com/140/140/any',
-        },
-      },
-    ]);
-  }, []);
-  const sendRequest = (m: IMessage) => {
+  const sendRequest = useCallback(async (m: IMessage) => {
     fetch(
       'https://console.dialogflow.com/v1/integrations/messenger/webhook/0bc163be-2619-40bb-a99d-32499efe36e7/sessions/webdemo-ff974fda-a864-5aca-7190-f8b83f087408?platform=webdemo',
       // 'https://dialogflow.googleapis.com/v2/projects/chatbot-test02-qum9/agent/sessions/ca228a8c-9490-51ed-5ade-c0a01c6c305a:detectIntent',
@@ -152,6 +155,8 @@ const App = () => {
                   text: parsed?.queryResult?.fulfillmentText,
                   user: {
                     _id: 2,
+                    avatar:
+                      'https://avatars.githubusercontent.com/u/8170782?s=280&v=4',
                   },
                   createdAt: new Date(),
                   _id: Date.now(),
@@ -164,15 +169,17 @@ const App = () => {
       .catch(error => {
         console.error(error);
       });
-  };
-
-  const onSend = useCallback((m: IMessage[]) => {
-    const filtered = m.filter(e => e.user._id === 1);
-    sendRequest(filtered[filtered.length - 1]);
-    setMessages(previousMessages => GiftedChat.append(previousMessages, m));
   }, []);
 
-  useEffect(() => {}, []);
+  const onSend = useCallback(
+    (m: IMessage[]) => {
+      const filtered = m.filter(e => e.user._id === 1);
+      sendRequest(filtered[filtered.length - 1]);
+      setMessages(previousMessages => GiftedChat.append(previousMessages, m));
+    },
+    [sendRequest],
+  );
+
   const onRecord = async () => {
     if (isStartedMicro) {
       await Voice.stop();
@@ -188,24 +195,36 @@ const App = () => {
 
   return (
     <SafeAreaView style={{flex: 1}}>
-      {showRecordButton && (
-        <Button
-          title={isStartedMicro ? 'Đang ghi âm...' : 'Bắt đầu ghi âm'}
-          onPress={onRecord}
-        />
-      )}
+      <Button title="Voice Record" onPress={onRecord} />
       <GiftedChat
         messages={messages}
         onSend={onSend}
         user={{
           _id: 1,
+          name: 'Me',
+          avatar:
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzIdr7HcfjPdwSAGPtaGRxlaM7d-PaAJdX9Q&usqp=CAU',
         }}
         textInputProps={{
           color: 'red',
         }}
       />
+      {isStartedMicro && (
+        <View style={[StyleSheet.absoluteFill, styles.container]}>
+          <Text style={styles.recordTxt}>Đang ghi âm...</Text>
+          <Button title="Dừng ghi âm" onPress={onRecord} />
+        </View>
+      )}
     </SafeAreaView>
   );
 };
 
 export default App;
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  recordTxt: {fontSize: 30, color: 'white', marginBottom: 40},
+});
